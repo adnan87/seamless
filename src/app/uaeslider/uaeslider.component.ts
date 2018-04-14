4859728c82ce3887012279edb92ad6ae7d84a624
adnan.khan <adnan.khan@wwindia.com>
2018-04-14 13:47:21 +0400

    changes to uae slider speed

--- src/app/uaeslider/uaeslider.component.ts
+++ src/app/uaeslider/uaeslider.component.ts
@@ -36,12 +36,12 @@ export class UaesliderComponent implements OnInit {
             this.carouselOne = {
               grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
               slide: 1,
-              speed: 3400,
-              interval: 3800,
+              speed: 6000,
+              interval: 5800,
               point: {
                 visible: false
               },
-              load: 2,
+              load: 1,
               touch: true,
               loop: true,
               custom: 'banner'
@@ -50,6 +50,7 @@ export class UaesliderComponent implements OnInit {
 
   }
   public myfunc(event: Event) {
+      alert('value reached')
       // carouselLoad will trigger this funnction when your load value reaches
       // it is helps to load the data by parts to increase the performance of the app
       // must use feature to all carousel
